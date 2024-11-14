8208c63c221d9facfa513c77f080870d42763e8a
elievelton <elievelton@gmail.com>
2024-11-13 22:08:37 -0300

    Update escolher nome do arquivo

--- baixarVideosYoutube/dowloadTube.py
+++ baixarVideosYoutube/dowloadTube.py
@@ -1,10 +1,12 @@
-import yt_dlp
+import os
+from yt_dlp import YoutubeDL
 
-def baixar_video(url, apenas_audio=False):
+def baixar_video(url, nome_arquivo, apenas_audio=False):
     caminho_diretorio = "/home/kali/Videos"
     
+    # Define o formato do nome do arquivo com base na entrada do usuário
     ydl_opts = {
-        'outtmpl': f'{caminho_diretorio}/%(title)s.%(ext)s',  # Define o formato do nome do arquivo
+        'outtmpl': f'{caminho_diretorio}/{nome_arquivo}.%(ext)s',
     }
 
     if apenas_audio:
@@ -18,17 +20,20 @@ def baixar_video(url, apenas_audio=False):
         ydl_opts['format'] = 'best'
 
     try:
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        with YoutubeDL(ydl_opts) as ydl:
             ydl.download([url])
-        print(f'Download concluído! Arquivo salvo em: {caminho_diretorio}')
+        print(f'Download concluído! Arquivo salvo em: {caminho_diretorio}/{nome_arquivo}')
     except Exception as e:
         print(f'Ocorreu um erro: {e}')
 
 # Solicitar a URL do vídeo
 video_url = input('Digite a URL do vídeo do YouTube: ')
 
+# Solicitar ao usuário o nome do arquivo
+nome_arquivo = input('Digite o nome do arquivo para salvar: ')
+
 # Solicitar ao usuário se ele deseja baixar apenas o áudio
 apenas_audio = input('Deseja baixar apenas o áudio? (s/n): ').lower() == 's'
 
 # Baixar o vídeo ou áudio com base na entrada do usuário
-baixar_video(video_url, apenas_audio)
+baixar_video(video_url, nome_arquivo, apenas_audio)
