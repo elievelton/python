7e9866b1d6d6ef1d526037f8987a767ea9609690
elievelton <elievelton@gmail.com>
2025-10-28 08:39:32 -0400

    Diversas alteracoes

--- speedTest/teste_de_velocidade.py
+++ speedTest/teste_de_velocidade.py
@@ -1,3 +1,9 @@
+
+# Autor: Elievelto Edimar da Silva
+# Data: 28/10/2025
+# Descrição: Teste de velocidade de internet com animação estilo encanto
+# Só para teste e treino
+
 from rich.console import Console
 from rich.progress import Progress, SpinnerColumn, TextColumn
 import speedtest
@@ -24,6 +30,7 @@ down_speed = down_speedt / 1_000_000
 up_speed_mbps = up_speed / 1_000_000
 
 # Exibindo os resultados com estilo
+console.print(f"\n")
 console.print(f"---------------------------------------------------------------")
 console.print(f"[bold green]Download Speed:[/bold green] {down_speed:.2f} Mbps")
 console.print(f"[bold blue]Upload Speed:[/bold blue] {up_speed_mbps:.2f} Mbps")
